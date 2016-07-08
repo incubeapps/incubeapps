b1d3b8e40ef00749e6105cfbb884b2febb273f2a
pyzen5 <pyzen55@gmail.com>
2016-07-08 20:29:23 +0530

    initial commit

--- portal/main.js
+++ portal/main.js
@@ -1,3 +1,22 @@
 /**
  * Created by Phani on 7/8/2016.
  */
+require.config({
+    paths:{
+        'angular':'lib/bower/angular/angular',
+        'angular-ui-router':'lib/bower/angular-ui-router/release/angular-ui-router/',
+        'domReady':'lib/require/domReady',
+        "jquery" : "lib/bower/jquery/dist/jquery",
+        "bootstrap" :  "lib/bower/bootstrap/dist/js/bootstrap.min"
+    },
+    shim:{
+        'angular-ui-router':['angular'],
+        'angular':{
+            exports:'angular'
+        },
+        "bootstrap" : { "deps" :['jquery'] }
+    },
+    deps:[
+        './bootstrap'
+    ]
+})
